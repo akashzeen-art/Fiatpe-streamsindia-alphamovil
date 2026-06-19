37d9ce0e041a036eaa7ee2db029f11c1b83c8925
Akash Sharma <akashzeen@gmail.com>
2026-06-19 16:12:17 +0530

    push

--- client/lib/media.ts
+++ client/lib/media.ts
@@ -1,2 +1,2 @@
 export const SITE_BG_VIDEO =
-  'https://vz-8af39f0e-519.b-cdn.net/85cd5368-8eb6-4210-aa7e-1464f618cd3d/play_720p.mp4';
+  'https://vz-8af39f0e-519.b-cdn.net/f90b2f93-19b1-4b54-833d-3b4ce10c6d2a/play_720p.mp4';
